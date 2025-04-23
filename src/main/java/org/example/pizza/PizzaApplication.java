3303ff4ad5bb21063d5a3b2533f632e9037f2f6e
licantropia <110132164+gcorona72@users.noreply.github.com>
2025-04-23 16:20:29 +0200

    Main

--- src/main/java/org/example/pizza/PizzaApplication.java
+++ src/main/java/org/example/pizza/PizzaApplication.java
@@ -1,13 +1,25 @@
 package org.example.pizza;
 
+import org.example.pizza.config.SupabaseConfig;
+import org.example.pizza.ui.LoginWindow;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import javax.swing.*;
 
 @SpringBootApplication
 public class PizzaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PizzaApplication.class, args);
-    }
+        // Iniciar Spring
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(PizzaApplication.class);
 
+        // Lógica Swing en Event Dispatch Thread
+        SwingUtilities.invokeLater(() -> {
+            LoginWindow login = context.getBean(LoginWindow.class);
+            login.setVisible(true);
+        });
+    }
 }
